c8355808411aef40e7db2a11fc4b7925790283ee
João <joaogabriel975@hotmail.com>
2025-03-12 13:51:26 -0300

    deleted parquet

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ PyInstaller.__main__.run([
     'main.py',  # seu arquivo principal
     '--onefile',  # criar um único executável
     '--noconsole',  # não mostrar console ao executar
-    '--add-data', 'dados.parquet;.',  # incluir arquivo de dados
     '--icon=icon.ico',  # se você tiver um ícone
     '--name=Retenção 5D',  # nome do executável
     '--windowed',  # aplicação com interface gráfica
